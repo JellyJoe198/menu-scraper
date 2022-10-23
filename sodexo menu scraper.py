2dba50fc7a92ab8d5151df259dd11ae0c21cbcde
Joseph Brownlee <39440458+JellyJoe198@users.noreply.github.com>
2022-10-23 14:50:49 -0600

    args to skip questions
    When running (such as from a shortcut) you can now add `-filter -skip` as arguments to skip the initial questions and go straight to the filtered data. It automatically asks the questions again when restarted within the app.

--- sodexo menu scraper.py
+++ sodexo menu scraper.py
@@ -11,6 +11,15 @@ import time
 import datetime as dt
 from bs4 import BeautifulSoup
 
+import sys
+
+true = True
+false = False
+
+print(sys.argv)
+SKIP_ASK = ("-skip" in sys.argv)
+ARG_FILTER = ("-filter" in sys.argv)
+
 ##URL = "https://realpython.github.io/fake-jobs/"
 URL = "https://menus.sodexomyway.com/BiteMenu/Menu?menuId=14978&locationId=75204001&whereami=http://minesdining.sodexomyway.com/dining-near-me/mines-market"
 print( "getting website data..." )
@@ -19,40 +28,81 @@ soup = BeautifulSoup(page.content, "html.parser")
 
 days = soup.find_all("div", class_="bite-day-menu")
 
+
+## level definitions:
+##  Time block {lunch, dinner, etc}
+##  Location {grillworks, Simple Servings, noodles, etc}
+##  Item {french fries, grilled cheese; chicken, rice, broccoli}
+
+def intAsk(query, inadd = " (1/0)"):
+    ## ask user and return integer value of the response
+    ## '0'+input prevents error by making it default to 0
+    return  int('0'+input(query + inadd) )
+
 while True: ## repeat parsing unless break
     
     today = dt.date.today()        
-    weekday = (today.weekday() +1) %7
+    weekday = (today.weekday() +1) %7 #today.isocalendar( int(now +1) % 7 ) ##day of week, sunday = 0
     while True:
         print("using", weekday, "as today's weekday.")
-        today = days[weekday]
+
+        try:
+            today = days[weekday] ##10/4/21: today (monday) is the second day of week shown.
+        except:
+            print("failed to get today's weekday.")
         ## todo: day of the month verification
-        print( "Today is", today.get("id") ,end='')
-        if int('0'+input(". Is this correct? (1/0)") ):
+        ## bug: month changes midweek? "IndexError: list index out of range"
+        print( "Today is", today.get("id") ,end=". ")
+        if SKIP_ASK or intAsk("Is this correct?"):
             break ## if user allows, continue program, otherwise change day.
         else:
-            ## '0'+input prevents error by making it default to 0
-            weekday = int( '0'+input("what day of the week is it? (assuming Sun=0) ") )
+            ## set day of week to user set value
+            weekday = intAsk("what day of the week is it? (assuming Sun=0) ", '') # %7
 
+    doFilter = true
+    allowedLocations = ["GRILL", "SIMPLE", "NOOD", "SOUP"] ## TITLES allowed in filter. must be ALLCAPS
+    if not ARG_FILTER:
+        print("default locations are ", allowedLocations)
+        doFilter = intAsk("Filter to only these locations?")
+
+    ## all times blocks in today's menu
     today_time_blocks = today.find_all( "div", class_="accordion-block" )
     for time_block in today_time_blocks:
         print('\n')
         title = time_block.find( "span", class_="accordion-copy" )
         print( title.text ,end="\n--------------------\n" ) # title of this time block, with a line for accent
 
+        ## for each location header in this time block
         locations = time_block.find_all( "h5" )
         for lotion in locations:
-            print()
-            print( lotion.text )
+            ## verify that location is allowed
+            if doFilter:
+                found = False;
+                for allowedLocation in allowedLocations:
+##                    print( allowedLocation, lotion.text )
+                    if (lotion.text.find( allowedLocation ) != -1):
+                        found = True
+##                        print("found")
+                        break #stop checking, we already found a match
+                
+                if not found:
+##                    print ("not allowed")
+                    continue ## stop running this location ## don't print things that aren't in allowed list
+
+            ## print items
+            print('\n', lotion.text ) ## location TITLE
             sector = lotion.find_next( "ul", class_="bite-menu-item" )
             items = sector( "div", class_="col-xs-9" ) # every menu item is in this same div class
             for item in items:
                 print( item.find("a").text )
 
-
-    if ( int('0'+input("\n\nrun again without reload? (1/0)")) == 0 ):
+    result = intAsk("\n\nrun again without reload?")
+    if ( result == 0 ):
         break ## if user inputs 0, end program, otherwise restart parsing
+    elif result == 1:
+        ARG_FILTER = false
+        SKIP_ASK = false
+        print ("\n"*5) ## whitespace for staying pretty
     else:
         print ("\n"*5)
-        ## go back to start of while loop
 
